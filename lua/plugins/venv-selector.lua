c11720adfe5a723cb7b481b4778fefd3a1a1300e
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-09-24 11:58:52 +0100

    feat: More plugin updates

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,6 +1,10 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "nvim-telescope/telescope.nvim",
+    "mfussenegger/nvim-dap-python",
+  },
   opts = {
     notify_user_on_activate = false,
   },
