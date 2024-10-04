b31502bcf1d09a1cddaf9a82645021a8ac531022
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-10-05 00:11:31 +0100

    feat: :sparkles: Added more plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,10 @@ keymap.set("n", "<leader>tT", function()
   neotest.run.run(vim.fn.getcwd())
   neotest.output_panel.open()
 end, { desc = "Run all tests and open output panel" })
+
+keymap.set(
+  "n",
+  "<leader>k",
+  '<cmd>lua require("kubectl").toggle()<cr>',
+  { noremap = true, silent = true, desc = "Toggle Kubectl" }
+)
