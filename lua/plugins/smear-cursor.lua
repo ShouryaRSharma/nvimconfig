4488f91a1528ad69688ecb973c8d4aafe3f26cbe
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-11-26 10:02:53 +0000

    chore: Removed smear between lines for frequent hjkl presses

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,10 +1,7 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    use_floating_windows = true,
-    stiffness = 0.8,
-    trailing_stiffness = 0.4,
-    trailing_exponent = 0.05,
-    distance_stop_animating = 0.3,
+    use_floating_windows = false,
+    smear_between_neighbor_lines = false,
   },
 }
