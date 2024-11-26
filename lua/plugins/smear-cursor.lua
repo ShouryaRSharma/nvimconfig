7c59d4f14caf1c0708a477e97011e5d781015e97
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-11-26 09:27:52 +0000

    feat: :sparkles: Added cursor trails

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    use_floating_windows = true,
+    stiffness = 0.8,
+    trailing_stiffness = 0.4,
+    trailing_exponent = 0.05,
+    distance_stop_animating = 0.3,
+  },
+}
