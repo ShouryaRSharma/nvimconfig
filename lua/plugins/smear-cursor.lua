795870a7be4f07a10070ba4d9c682cae9cb97ca9
Shourya Sharma <shourya.sharma@complyadvantage.com>
2024-12-27 11:44:59 +0000

    chore: Removed weird scrolling lazyvim added (yuck)

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -2,6 +2,7 @@ return {
   "sphamba/smear-cursor.nvim",
   commit = "99f73a5",
   opts = {
+    croll_buffer_space = false,
     use_floating_windows = false,
     smear_between_neighbor_lines = false,
   },
