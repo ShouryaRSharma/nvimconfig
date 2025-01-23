9ea9249303501eb8f462be63396ac1e3921d0b29
Shourya Sharma <shourya.sharma@complyadvantage.com>
2025-01-23 09:59:18 +0530

    feat: :sparkles: Added new keymap to remove whitespace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Add local clear whitespace function
+local function clear_whitespace()
+  vim.cmd(":%s/\\s\\+$//e")
+  vim.cmd(":nohl")
+end
+
 local keymap = vim.keymap
 
 -- Add Neogit keymap
@@ -27,3 +34,8 @@ end, { desc = "Load coverage" })
 keymap.set("n", "<leader>ct", function()
   vim.cmd("CoverageToggle")
 end, { desc = "Toggle coverage" })
+
+-- Keymap to clear trailing whitespaces
+keymap.set("n", "<leader>z", function()
+  clear_whitespace()
+end, { desc = "Clear trailing whitespaces" })
