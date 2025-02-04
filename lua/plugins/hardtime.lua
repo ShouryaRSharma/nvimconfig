745936036667ee67012a6da7c594a00af33880a6
Shourya Sharma <shourya.sharma@complyadvantage.com>
2025-02-04 12:39:24 +0530

    chore: Removed harditme limitation for debugging

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,5 +1,15 @@
 return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
+  opts = {
+    disabled_filetypes = {
+      "dap-repl",
+      "dapui_console",
+      "dapui_scopes",
+      "dapui_stacks",
+      "dapui_watches",
+      "dapui_breakpoints",
+    },
+    restriction_mode = "block",
+  },
 }
